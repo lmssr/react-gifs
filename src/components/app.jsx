7e1f769dd2dd73d86df0696994d684de9355ac7b
Luc Mosser <luc.mosser86@orange.fr>
2019-10-01 11:31:14 +0200

    nothing works

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,26 @@
-import React from "react"
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
 
-import Header from "./components/gif"
-import MainContent from "./components/giflist"
-import Footer from "./components/searchbar"
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
 
-function App() {
-    return (
-        <div>
-            <Gif />
-            <Giflist />
-            <Searchbar />
-        </div>
-    )
+const GIF_API_KEY = 'koZqacoIGEzGtON1Qfix924ZoZ4eQwMY';
+
+class App extends Component {
+  constructor(props) { // be promoted into a class
+    super(props);
+
   }
 
-export default App
+  render() {
+    return (
+        <div>
+            <SearchBar searchFunction={this.search}/>
+            <Gif id={this.state.selectedGifId} />
+        </div>
+    );
+  }
+}
+
+export default App;
